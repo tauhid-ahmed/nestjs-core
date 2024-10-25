472ef686ae5ece42efdc80e4c034101f0521ddf8
Tauhid <tauhidahmed716@gmail.com>
2024-10-25 23:36:10 +0600

    removes coffees controller fom app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CoffeeController } from './coffees/coffees.controller';
 import { CoffeeModule } from './coffees/coffees.module';
 
 @Module({
   imports: [CoffeeModule],
-  controllers: [AppController, CoffeeController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
