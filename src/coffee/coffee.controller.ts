b33e4d44d474d9c5e0c4a54fcb5e853ffed84274
Tauhid <tauhidahmed716@gmail.com>
2024-10-25 23:20:57 +0600

    deleted old coffee module files
    - for re creating for better understanding..
    - prev version has testing support. removes testing support from now on

--- src/coffee/coffee.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller('coffee')
-export class CoffeeController {
-  @Get()
-  getCoffee() {
-    return 'coffee';
-  }
-}
--- src/coffee/coffee.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CoffeeController } from './coffee.controller';
-import { Coffee } from './coffee';
-
-@Module({
-  controllers: [CoffeeController],
-  providers: [Coffee]
-})
-export class CoffeeModule {}
