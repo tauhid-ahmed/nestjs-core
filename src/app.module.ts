983070488e2c71a451f7122998f36ff5598c367a
Tauhid <tauhidahmed716@gmail.com>
2024-10-25 23:22:04 +0600

    inject coffee module to main app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CoffeeModule } from './coffee/coffee.module';
+import { CoffeeController } from './coffees/coffees.controller';
+import { CoffeeModule } from './coffees/coffees.module';
 
 @Module({
   imports: [CoffeeModule],
-  controllers: [AppController],
+  controllers: [AppController, CoffeeController],
   providers: [AppService],
 })
 export class AppModule {}
